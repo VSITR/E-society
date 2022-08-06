c1330e5a02891c25031311acdb1efd24e96d9a77
Nisarg Shah <nshah@capritechnosys.com>
2022-08-06 23:28:43 +0530

    remvdusecreateindex,usefindandmodifyasnotsupported

--- config/db.js
+++ config/db.js
@@ -1,17 +1,15 @@
 const mongoose = require('mongoose');
 
-exports.connectDB = async() => {
-    try{
+exports.connectDB = async () => {
+    try {
         const connection = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true, 
-            useUnifiedTopology: true,
-            useCreateIndex: true,
-            useFindAndModify: false
+            useNewUrlParser: true,
+            useUnifiedTopology: true
         });
-        console.log("MongoDB connected: "+connection.connection.host)
+        console.log("MongoDB connected: " + connection.connection.host)
     }
-    catch(error){
-        console.error("Error "+error.message);
+    catch (error) {
+        console.error("Error " + error.message);
         process.exit(1);
     }
 }
